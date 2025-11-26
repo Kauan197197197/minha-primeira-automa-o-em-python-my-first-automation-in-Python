963073432609e6ff24a6bad358514319d3586683
kauan <krcandido2009@gmail.com>
2025-11-26 18:19:49 -0300

    Add files via upload

---
+++ xer.py
@@ -0,0 +1,17 @@
+import pyautogui
+import time
+pyautogui.PAUSE = 2
+
+pyautogui.press("win")
+pyautogui.write("chome")  
+pyautogui.press("enter") 
+pyautogui.press("enter") 
+pyautogui.write("youtube.com")
+
+pyautogui.press("enter")
+pyautogui.click(x=515, y=134)
+
+
+pyautogui.write("aula de python")
+
+pyautogui.press("enter")   
